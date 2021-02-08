11d0b85c45c073f48af4693ee58d3c5cd113eff3
Dmitrii <lifesider7@gmail.com>
2021-02-08 14:46:48 +0400

    Update journal.py

--- journal.py
+++ journal.py
@@ -5,36 +5,36 @@ To further open in Exel as CSV (separator ;)
 Exemple run $./journal.py <input_file>
 '''
 import sys
-fr_name = sys.argv[1]
+fr_name = sys.argv[0]
 fw_name = 'table_' + fr_name
-fr = open(fr_name,'r')
-fw = open(fw_name,'w')
-for line in fr :
+fr = open(fr_name, 'r')
+fw = open(fw_name, 'w')
+for line in fr:
     w_line = ''
     data = line[0:10]
     nkm = line[11:27]
-    if len(line) > 36 : #activation
-        if line[-6].isdigit() : #nomer a000aa000
+    if len(line) > 36:  # activation
+        if line[-6].isdigit() :  # nomer a000aa000
             nom = line[-12:-1]
-            if line[-14].isdigit() : # vin
+            if line[-14].isdigit():  # vin
                 vin = line[-30:-13]
                 org = line[29:-30]
-            else : #no vin
+            else:  # no vin
                 vin = '-'
                 org = line[29:-13]
-        else : #nomer a000aa00
+        else:  # nomer a000aa00
             nom = line[-11:-1]
             if line[-13].isdigit() : #vin
                 vin = line[-29:-12]
                 org = line[29:-29]
-            else : #no vin
+            else:  # no vin
                 vin = '-'
                 org = line[29:-12]
         w_line = data + ';' + nkm + ';' + org + ';' + vin + ';' + nom + '\n'
-    else : #deactivation
+    else:  # deactivation
         w_line = data + ';' + nkm + '; ; ; ' + '\n'
     fw.write(w_line)
-print 'Complite OK'
+print('Complete OK')
 fr.close()
 fw.close()
 exit()
