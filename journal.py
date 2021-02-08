312e79647b43f81d05521a1084b13af1954aecb7
Dmitrii <lifesider7@gmail.com>
2021-02-08 15:27:09 +0400

    Update journal.py

--- journal.py
+++ journal.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-'''
-Script for convert file (input_file)
-To further open in Exel as CSV (separator ;)
-Exemple run $./journal.py <input_file>
-'''
+# Script for convert file (input_file)
+# To further open in Exel as CSV (separator ;)
+# Example run $./journal.py <input_file>
+
 import sys
+
 fr_name = sys.argv[1]
 fw_name = 'table_' + fr_name
 fr = open(fr_name, 'r')
@@ -14,7 +13,7 @@ for line in fr:
     data = line[0:10]
     nkm = line[11:27]
     if len(line) > 36:  # activation
-        if line[-6].isdigit() :  # nomer a000aa000
+        if line[-6].isdigit():  # nomer a000aa000
             nom = line[-12:-1]
             if line[-14].isdigit():  # vin
                 vin = line[-30:-13]
@@ -24,7 +23,7 @@ for line in fr:
                 org = line[29:-13]
         else:  # nomer a000aa00
             nom = line[-11:-1]
-            if line[-13].isdigit() : #vin
+            if line[-13].isdigit():  # vin
                 vin = line[-29:-12]
                 org = line[29:-29]
             else:  # no vin
